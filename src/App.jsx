97b2bb802cf2718a3eb4d723571b561450dfd489
Bianco <bianco.emiliozzi@gmail.com>
2020-07-16 15:15:05 -0300

    :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,63 @@
 import React, { useState, useEffect } from "react";
 import twitterLogo from "./images/twitterLogo.png";
 import styled from "styled-components";
-
-const getRandomNumber = (maxNum) => {
-  return Math.floor(Math.random() * maxNum);
-};
-const getRandomColor = () => {
-  return `rgb(${getRandomNumber(256)}, ${getRandomNumber(
-    256
-  )}, ${getRandomNumber(256)})`;
-};
+import { useHistory } from "react-router-dom";
+import { getRandomColor } from "./utils.js";
+import ChangingButton from "./components/ChangingButton";
+import SyncLoader from "react-spinners/SyncLoader";
 
 function App() {
   const [quote, setQuote] = useState("");
+  const [quoteDetails, setQuoteDetails] = useState("");
   const [randomColor, setRandomColor] = useState("");
   const [triggerFetch, setTriggerFetch] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
   useEffect(() => {
     const getQuote = () => {
       return fetch("http://localhost:5000/newQuote")
         .then((res) => res.json())
         .then((data) => {
-          setQuote(data.result);
+          const { quote, author, subject, carrerYear, text } = data;
+          const { name, link } = text;
+          setQuote(`'${quote}' - ${author}`);
+          setQuoteDetails([subject, carrerYear, name, link]);
         });
     };
     getQuote();
     setRandomColor(getRandomColor());
   }, [triggerFetch]);
 
+  const history = useHistory();
+
+  const changeButton = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const contentToShow = () => {
+    if (showDetails) {
+      return (
+        <ChangingButton
+          changeButton={changeButton}
+          color={randomColor}
+          content={quoteDetails}
+        />
+      );
+    } else {
+      return (
+        <ChangingButton
+          changeButton={changeButton}
+          color={randomColor}
+          content={[quote]}
+        />
+      );
+    }
+  };
+
   return (
     <Wrapper color={randomColor}>
-      <Quote>
-        {quote ? <Text color={randomColor}>{quote}</Text> : "Cargando..."}
+      <Container>
+        {quote ? contentToShow() : <SyncLoader size={30} color={randomColor} />}
         <ButtonsGroup>
           <LinkButton
             href={`https://twitter.com/intent/tweet?text=${quote}`}
@@ -48,30 +74,38 @@ function App() {
             Otra cita
           </Button>
         </ButtonsGroup>
-      </Quote>
+      </Container>
+      <Button
+        onClick={() => history.push("/nuevacita")}
+        bgColor={randomColor}
+        color="whitesmoke"
+      >
+        Contribuí con una cita
+      </Button>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div({
-  alignItems: "center",
-  backgroundColor: (props) => props.color,
   display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-evenly",
+  alignItems: "center",
   height: "100vh",
-  justifyContent: "center",
+  backgroundColor: (props) => props.color,
 });
-const Quote = styled.div({
+const Container = styled.div({
   backgroundColor: "whitesmoke",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  textAlign: "center",
   borderRadius: "5px",
   padding: "1%",
-  textAlign: "center",
   width: "40%",
+  minHeight: "50vh",
 });
-const Text = styled.h2({
-  color: (props) => props.color,
-  fontSize: "25px",
-});
-const ButtonsGroup = styled.section({
+const ButtonsGroup = styled.div({
   display: "flex",
   justifyContent: "space-evenly",
   alignItems: "center",
@@ -80,12 +114,13 @@ const LinkButton = styled.a({
   textDecoration: "none",
 });
 const Button = styled.button({
-  background: "none",
+  backgroundColor: (props) => props.bgColor || "whitesmoke",
   border: (props) => `1px solid ${props.color}`,
   color: (props) => props.color,
   borderRadius: "5px",
   cursor: "pointer",
-  padding: "1%",
+  padding: "4px",
+  boxShadow: "inset 0px 0px 3px white",
 });
 const TwitterLogo = styled.img({
   width: "20px",
